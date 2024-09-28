311dbc18faf5a314bafc219b3cdafe5fbb99cd94
muzamil1233 <muzamiln213@gmail.com>
2024-09-28 23:04:26 +0530

    added

---
+++ src/Components/interview qns/Mock3.jsx
@@ -0,0 +1,60 @@
+// html basically used to describe the content and structure of webpage 
+
+// sementic tags are tags which describe its meaning to the browser or web developer eg header <footer>
+// </footer> in search engine  and optimizatiion
+
+
+// element is tag with its content included  <div> <p> kehsa bouy</p></div>
+// tag is just the tag with its content  <p>  how are  you </p>
+
+
+
+// attributes provide the additional information about the elemnts or tags and they are included in opening tag
+
+
+// div is block level  element  it takes full  width of the parent it is in
+// span is a inline element it takes the width of content  eg span    img tag  
+
+// block level elemnt always start fro new line 
+// and inline should start from its line
+
+
+// inline elements are two types replacable inline elements and nonrepresable 
+// the elemnts whho come from any url are replacable elemts and we can give some height and width but in nonrepresable we cant give 
+
+// cascading means the priority we are giving to the styles on which order the styles are 
+// it is the  order on which css is given to the styesles
+
+// specificity are the priority given to the selectors the  selectors which are higher specificity will be given the stles first
+
+
+// the defualt value of position is static
+
+// relative positioning means it does not change the position of the element but it will create a new position relative to the original position of the element
+// relative position is relative to the normal static posiytion 
+// and absolute position itself  acc to ancestral elemts position 
+
+// flexbox is a layout system that is used in css for defining the layout of webpage
+
+
+// box model is a layout system that includes  content padding margin border;
+
+
+// box sizing default value is content box 
+// content-box: This is the default value of box-sizing. The dimension of element only includes ‘height’ and ‘width’ and does not include ‘border’ and ‘padding’ given to element. Padding and Border take space outside the element.
+
+// border-box = size of content + padding + border
+
+// javascript makes the web pages are interactive with dynamic content 
+
+// 8 datatypes and 1 are primitive 
+// var is functional scope variable 
+
+// parameter and arguments 
+
+// parameter are passed at function definition
+// arguments  are the actual values when we call the functin 
+
+// Slice() creates a new array by copying elements from an existing array without altering the old array
+// Splice() directly mutates the original array by inserting, deleting or replacing elements at a specified index.23 Jul 2024
+// reactbis declirative approach and js is imperative 
